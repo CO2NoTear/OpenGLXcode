905e43b255c1edb608c284fcce3dd2736fcc9fb9
CO2NoTear <sqede@outlook.com>
2023-09-05 17:23:30 +0800

    Initial Commit

---
+++ GLTEST/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  GLTEST
+//
+//  Created by 孙权恩 on 2023/9/5.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
